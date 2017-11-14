0a407a37f1862f237b2f34d1aadd7d224f775bf2
ivanr553 <ivanr553@gmail.com>
2017-11-13 20:58:31 -0800

    changed wording in file

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('XMLHttpRequest').XMLHttpRequest
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 var xmlHttp = new XMLHttpRequest()
 xmlHttp.open( "GET", "https://kmp-api.herokuapp.com/run", false )
