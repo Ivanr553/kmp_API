e9946f10cc27cc1a5755611bf38fec371473c87c
Sergei <sergei@Roberts-MBP.attlocal.net>
2017-11-23 21:50:39 -0800

    rerouted db to sergei's db

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://kmp:kmp123@ds111565.mlab.com:11565/kmpdb'
+  //db: 'mongodb://kmp:kmp123@ds111565.mlab.com:11565/kmpdb',
+  db: 'mongodb://kmpsd:kmpsd123@ds117336.mlab.com:17336/kmp'
 }
